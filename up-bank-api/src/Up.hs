08c0f7b69f70c97ffba9e47032743442590cf2b4
Leo Orpilla III <leoorpilla3@gmail.com>
2021-03-28 19:14:52 +1000

    Fix #2

--- up-bank-api/src/Up.hs
+++ up-bank-api/src/Up.hs
@@ -22,11 +22,12 @@ upBaseUrl = BaseUrl Https "api.up.com.au" 443 "/api/v1"
 
 -- | Adds the Authorization header to every transaction
 makeUpClientRequest :: Token -> BaseUrl -> Request -> Client.Request
-makeUpClientRequest (Token token) = (addHeader .) . defaultMakeClientRequest
+makeUpClientRequest token = (addHeader .) . defaultMakeClientRequest
     where 
-          addHeader req = req {Client.requestHeaders = ("Authorization", BSC.pack $ token) : Client.requestHeaders req}
+          addHeader req = req {Client.requestHeaders = ("Authorization", BSC.pack $ tokenToString token) : Client.requestHeaders req}
 
 mkUpClient :: Token -> IO ClientEnv
 mkUpClient t = do
   mgr <- Client.newManager tlsManagerSettings
   pure (mkClientEnv mgr upBaseUrl) { makeClientRequest = makeUpClientRequest t }
+ 
